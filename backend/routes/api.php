2b799a55f8e5e4c2edab593a978e840d02e446a7
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2026-06-19 17:44:56 +0200

    Delete test controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../controllers/TestController.php';
-
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
@@ -10,10 +8,12 @@ header("Access-Control-Allow-Headers: Content-Type");
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($uri === '/test')
+if ($uri === '/')
 {
-    $controller = new TestController();
-    echo json_encode($controller->index());
+    echo json_encode([
+        'description' => 'This is the api endpoint.',
+        'version' => '1.0.0',
+    ]);
 }
 
 
