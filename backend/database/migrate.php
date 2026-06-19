1533fdd7fc053a2588431e818a3b304f236d09d1
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2026-06-19 21:23:10 +0200

    create a migration file, handing the base datatable creation

--- backend/database/migrate.php
+++ backend/database/migrate.php
@@ -1,10 +1,24 @@
 <?php
 
 require __DIR__ . '/Database.php';
-$user = require __DIR__ . '/migrations/001_create_user_table.php';
 
 try {
-    $user(new Database()->connect());
+    $dir = new DirectoryIterator(__DIR__ . '/migrations/.');
+    $migrations = [];
+    foreach ($dir as $fileInfo) {
+        if ($fileInfo->isDot() || $fileInfo->getExtension() !== 'php') {
+            continue;
+        }
+        $migrations[] = require $fileInfo->getPathname();
+    }
+
+    $pdo = new Database()->connect();
+
+    foreach ($migrations as $migration) {
+        $migration($pdo);
+    }
+
+    echo 'Done';
 } catch (Exception $e) {
     echo $e->getMessage();
 }
